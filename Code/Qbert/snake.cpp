9a6019eba3bc9d3403b60ac9138ebc3ad9181d3d
CorbinNanneman <35775531+CorbinNanneman@users.noreply.github.com>
2018-03-22 11:43:47 -0500

    Snake make bank
    the snake is done and it works

--- Code/Qbert/snake.cpp
+++ Code/Qbert/snake.cpp
@@ -20,7 +20,7 @@ __int8 Snake::update(float fpsScale, __int16 screenWidth, float scale, __int16 f
 
 		if (getRow() == 6)
 		{
-			setTexture("./images/snake.png", 15, 32);
+			setTexture("./images/snake.png", 16, 32);
 			isEgg = false;
 		}
 	}
@@ -55,8 +55,20 @@ __int8 Snake::update(float fpsScale, __int16 screenWidth, float scale, __int16 f
 		}
 		else
 		{
-
-			Character::move(rand() % 4, scale, fpsScale);
+			if (getRow() == 6)
+			{
+				if (qbertX > GameObject::getX())
+					Character::move(0, scale, fpsScale);
+				else
+					Character::move(3, scale, fpsScale);
+			}
+			else
+			{
+				if (qbertX > GameObject::getX())
+					Character::move(rand() % 2, scale, fpsScale);
+				else
+					Character::move(rand() % 2 + 2, scale, fpsScale);
+			}	
 			//move up or down
 		}
 
